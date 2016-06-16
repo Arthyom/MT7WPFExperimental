db396f80a308e1a36615ff22e1a18b06cb9aad42
arthyom <allpipiasaaa@hotmail.com>
2016-06-15 19:59:52 -0500

    explorando mas del wpf, alineacion, visibilidad etc

--- WPFMultitoqueExperimental/WPFMultitoqueExperimental/MainWindow.xaml.cs
+++ WPFMultitoqueExperimental/WPFMultitoqueExperimental/MainWindow.xaml.cs
@@ -29,5 +29,31 @@ namespace WPFMultitoqueExperimental
         {
             MessageBox.Show("haciendo Clisk");
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // sin embargo tambien se pueden crear objetos desde el codigo de c#
+            ListBox L1 = new ListBox();
+            L1.Height = 50;
+            L1.Width = 50;
+
+            L1.Background = new SolidColorBrush(Color.FromArgb(194, 34, 23, 43));
+
+            ListBoxItem it =  new ListBoxItem();
+            ListBoxItem it2 = new ListBoxItem();
+            ListBoxItem it3 = new ListBoxItem();
+
+            it.Content = "hola";
+            it2.Content = "adios";
+            it3.Content = "bye";
+
+            L1.Items.Add(it);
+            L1.Items.Add(it2);
+            L1.Items.Add(it3);
+
+            L1.HorizontalAlignment = HorizontalAlignment.Left;
+            this.grid.Children.Add(L1);
+
+        }
     }
 }
