43e5c6427bec9940c8ce4b48bc5a7fa996061980
arthyom <allpipiasaaa@hotmail.com>
2016-06-21 15:47:26 -0500

    poniendo todo junto

--- WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window6.xaml.cs
+++ WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window6.xaml.cs
@@ -22,5 +22,26 @@ namespace WPFMultitoqueExperimental
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (Control c in this.g1.Children)
+            {
+                if (c is Label || c is Button)
+                {
+                    // cambiar color de botones 
+                    c.Foreground = Brushes.White;
+
+                    // cambiar el color de fuente 
+                    c.FontFamily = new FontFamily("Arial");
+
+                    //cambiar las dimenciones de la fuente
+                    c.FontSize = 12;
+
+                    // cambiar el estilo de la fuente
+                    c.FontWeight = FontWeight.FromOpenTypeWeight(22);
+                }
+            }       
+        }
     }
 }
