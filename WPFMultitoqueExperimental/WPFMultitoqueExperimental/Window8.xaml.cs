73bb8e8a7d6f88cf8160df9b1be3558df5914abc
arthyom <allpipiasaaa@hotmail.com>
2016-06-21 19:38:53 -0500

    Experimentando con el dibujo multitactil

--- WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window8.xaml.cs
+++ WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window8.xaml.cs
@@ -10,14 +10,20 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Ink;
 
 namespace WPFMultitoqueExperimental
 {
     /// <summary>
     /// Interaction logic for Window8.xaml
     /// </summary>
+    /// 
+    
+
     public partial class Window8 : Window
     {
+        Point P1, P2;
+
         public Window8()
         {
             InitializeComponent();
@@ -28,5 +34,47 @@ namespace WPFMultitoqueExperimental
         {
             area.Cursor = Cursors.Cross;
         }
+
+        // generar un punto cuando el raton se precione
+        private void Precionar (object sender, MouseButtonEventArgs e)
+        {
+            this.P1 = e.GetPosition(area);
+
+        }
+
+        // conseguir el punto de soltura 
+        private void Soltar (object sender, MouseButtonEventArgs e)
+        {
+            this.P2 = e.GetPosition(area);
+
+            // dibujar una linea recta entre los dos puntos 
+            Line l1 = new Line();
+            l1.X1 = this.P1.X;
+            l1.Y1 = this.P1.Y;
+
+            l1.X2 = this.P2.X;
+            l1.Y2 = this.P2.Y;
+
+            l1.Stroke = Brushes.Black;
+            l1.StrokeThickness = 2;
+
+            area.Children.Add(l1);
+        }
+
+        // cambiar color aleatoriamente al stroke
+        private void CambiarColor ( object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("cambiando color");
+            Random rn = new Random();
+
+            // usar las propiedades de dibujo para crear un color nuevo
+            DrawingAttributes AtributosDibujo = new DrawingAttributes();
+            AtributosDibujo.Color = Color.FromRgb(Convert.ToByte( rn.Next(255)) ,Convert.ToByte( rn.Next(255) ), Convert.ToByte( rn.Next(255)) );
+            area.DefaultDrawingAttributes = AtributosDibujo;
+            
+            
+        }
+
+
     }
 }
