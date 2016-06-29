7c1b26b11b640419ed33084ff9199e7f3c25073e
arthyom <allpipiasaaa@hotmail.com>
2016-06-28 20:16:26 -0500

    simulando el toque con el raton

--- WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window11.xaml.cs
+++ WPFMultitoqueExperimental/WPFMultitoqueExperimental/Window11.xaml.cs
@@ -33,11 +33,39 @@ namespace WPFMultitoqueExperimental
         */
         Dictionary<MouseDevice, Ellipse> Seguidor = new Dictionary<MouseDevice, Ellipse>();
 
+        Point currentPoint = new Point();
+
+
+
         public Window11()
         {
             
 
             
+        }
+
+        private void Canvas_MouseDown_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(CanvasTouch);
+        }
+
+        private void Canvas_MouseMove_1(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition(CanvasTouch).X;
+                line.Y2 = e.GetPosition(CanvasTouch).Y;
+
+                currentPoint = e.GetPosition(CanvasTouch);
+
+                CanvasTouch.Children.Add(line);
+            }
         }
 
         public void PrecionarRaton_MouseDown ( object sender, MouseButtonEventArgs e)
